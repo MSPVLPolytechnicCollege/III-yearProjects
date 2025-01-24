73beabccce4744c3af15d9232c629ac4a0446bb5
projecteams004 <charitytrust244@gmail.com>
2025-01-24 19:49:28 +0530

    chgeinform

--- app.py
+++ app.py
@@ -1,28 +1,65 @@
 
+#import sqlite
 import sqlite3
 
-from click import confirm
-from flask import Flask,render_template,request,flash,redirect,url_for,session
+from flask import Flask, render_template, request, redirect, url_for,session
 
 app=Flask(__name__)
+app.secret_key="123"
 
 @app.route('/')
 @app.route('/loginpage')
 def home():
     return render_template('loginpage.html')
 
-app.route('/confirm',methods=['GET','POST'])
-def insert():
+
+@app.route('/register',methods=['GET','POST'])
+def register():
     if request.method=="POST":
-            u=request.form.get('uname')
-            p = request.form.get('password')
-            con=sqlite3.connect('lakshmi1.db')
-            cur=con.cursor()
-            cur.execute("insert into ut1 (uname,password)values(?,?);",(u,p))
-            con.commit()
-            con.close()
-            return render_template('confirm.html')
+        n = request.form['name']
+        e = request.form['email']
+        m = request.form['mobile']
+        pw = request.form['password']
+        con = sqlite3.connect('charity.db')
+        cursor = con.cursor()
+        cursor.execute("insert into register(name,email,mobile_no,password) values(?,?,?,?);", (n, e, m, pw))
+        con.commit()
+        con.close()
+    return render_template('register.html')
+    #return redirect (url_for('loginpage.html'))
+
+#login page code
+
+@app.route('/confirm',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        u = request.form['name']
+        p = request.form['password']
+        con = sqlite3.connect('charity.db')
+        #con.row_factory=sqlite3.Row
+        cursor = con.cursor()
+        #cursor.execute("insert into lgn(uname,password) values(?,?);", (u, p))
+        cursor.execute("select * from register where name=? and password=?", (u,p))
+        #data=cursor.fetchone()
+
+        """if data:
+            session["name"]= data["name"]
+            session["password"] = data["password"]
+            return redirect("sucess.html")
+        else:
+            print("username and password missmatch")
+    return redirect(url_for('loginpage.html'))
+        #con.commit()
+        #con.close()"""
+        return render_template('confirm.html')
     return redirect (url_for('loginpage.html'))
+
+@app.route('/sucess',methods=['GET','POST'])
+def sucess():
+    return render_template("sucess.html")
+
+
+
 
 
 
