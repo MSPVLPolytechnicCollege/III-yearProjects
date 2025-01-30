0795193c100f2cb3ad13e04c6fae4cf31bae40b2
nanthu321 <rnanthakumar00z@gmail.com>
2025-01-30 23:05:38 +0530

    manage claases file added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("login.html")
+    return render_template("index.html")
 
 @app.route("/signup")
 def signup():
@@ -45,6 +45,7 @@ def create_attendence():
     data = cur.fetchall()
     con.close()
     return render_template("all_attendance.html", data=data)
+
 
 
 @app.route("/all_attendance")
