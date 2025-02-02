0f4b499730ae8d54556d18ed71f3e8cee8036468
nanthakumar0224 <ce23591082@gmail.com>
2025-02-02 12:25:37 +0530

    msg

--- main.py
+++ main.py
@@ -73,16 +73,15 @@ def login_users():
 
 
 
-@app.route("/manage_classes")
+@app.route("/manage_classes",methods=["POST", "GET"])
 def manage_classes():
     con = sqlite3.connect('data_base.db')
     cur = con.cursor()
     cur.execute("Select * from all_classes")
     data = cur.fetchall()
     con.close()
-    return render_template("manage_classes.html", data=data)
-  
-
+    #return render_template("manage_classes.html",data=data)
+    return data
 
 
 
