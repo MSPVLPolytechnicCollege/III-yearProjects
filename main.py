fc216382a14ed9a74c5150f98d001ea8ad3573a6
nanthakumar0224 <ce23591082@gmail.com>
2025-02-09 21:53:24 +0530

    Admin staff and other folder added

--- main.py
+++ main.py
@@ -5,35 +5,38 @@ import pandas as pd
 app = Flask(__name__)
 
 
+# home
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("login.html")
 
 
 
-
+#sign up
 @app.route("/signup")
 def signup():
     return render_template('signup.html')
 
 
 
-
-@app.route("/login")
+#return login page
+@app.route("/login",methods=["POST","GET"])
 def login():
-    return render_template('login.html')
+    return render_template("login.html")
 
 
 
 
 
-
+#insert user
 @app.route("/insert_users",methods=["POST","GET"])
 def insert_users():
+    in_mode = request.form['select']
     in_username = request.form['user_name']
     in_password = request.form['password']
     in_confirmpass = request.form['confirmpassword']
+
 
     if in_password == in_confirmpass:
         con = sqlite3.connect("data_base.db")
@@ -45,11 +48,13 @@ def insert_users():
         if data:
             return "user already exist"
         else:
-            cur.execute("INSERT INTO users(username,password) VALUES(?,?)", (in_username, in_password))
+            cur.execute("INSERT INTO users(mode,username,password) VALUES(?,?,?)", (in_mode,in_username, in_password))
             con.commit()
             con.close()
             return render_template("login.html")
+
     else:
+
         return "password not match"
 
 
@@ -58,15 +63,20 @@ def insert_users():
 
 @app.route('/login_users',methods=["POST","GET"])
 def login_users():
+    in_mode = request.form['select']
     user_name = request.form['user_name']
     password = request.form['password']
     con = sqlite3.connect("data_base.db")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("Select * from users where username=? and password=?",(user_name,password))
+    cur.execute("Select * from users where username=? and password=? and mode=?",(user_name,password,in_mode))
     data = cur.fetchall()
-    if data:
-        return render_template('index.html')
+    if data and in_mode == "admin":
+        return render_template("admin/index.html")
+    elif data and in_mode == "other":
+        return render_template("other/index.html")
+    elif data and in_mode == "staff":
+        return render_template("staff/index.html")
     else:
         return "invalid user or password"
     
@@ -81,8 +91,18 @@ def manage_classes():
     cur.execute("Select * from all_classes")
     data = cur.fetchall()
     con.close()
-    return render_template("manage_classes.html", data=data)
+    return render_template("admin/manage_classes.html", data=data)
     
+
+@app.route("/manage_classes_staff",methods=["POST", "GET"])
+def manage_classes_staff():
+    con = sqlite3.connect("data_base.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("Select * from all_classes")
+    data = cur.fetchall()
+    con.close()
+    return render_template("staff/manage_classes.html", data=data)
 
 
 
@@ -96,185 +116,69 @@ def create_class():
         in_classteacher = request.form['classteacher'] 
         con = sqlite3.connect('data_base.db')
         cur = con.cursor()
-        query = f"CREATE TABLE {in_classname} (in_classname varchar(50), in_classid integer primary key, in_classteacher varchar(100))"
+        query = f"CREATE TABLE {in_classname} (rollno integer,name varchar(50),m1 integer,m2 integer,m3 integer)"
         cur.execute("INSERT INTO all_classes(classname,classid,classteacher) VALUES(?,?,?)",(in_classname,in_classid,in_classteacher))
         cur.execute(query)
         con.commit()
         con.close()
-        return "inserted successfully"
+        return "class created and inserted successfully"
     except:
         return "Class name or Id already exist"
 
- 
-@app.route('/add_stud')
-def add_stud():
-    return render_template('add_stud.html')
-        
+
+
+@app.route("/attendance/<string:classname>",methods=["POST", "GET"])
+def attendance(classname):
+    con = sqlite3.connect("data_base.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    query = f"SELECT * FROM {classname}"
+    cur.execute(query)
+    data = cur.fetchall()
+    con.close()
+    return render_template("admin/add_stud.html",data=data,classname=classname)
+
 @app.route('/excel_to_table',methods=["POST", "GET"])
 def excel_to_table():
+    in_classname = request.form['classname']
     file = request.files['file']
     df = pd.read_excel(file)
     conn = sqlite3.connect("data_base.db")
-    df.to_sql('attendance', conn, if_exists='append', index=False)
+    df.to_sql(in_classname, conn, if_exists='append', index=False)
     conn.commit()
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
-    cur.execute("Select * from attendance")
+    query = f"SELECT * FROM {in_classname}"
+    cur.execute(query)
     data = cur.fetchall()
 
-    return render_template("add_stud.html",data=data)
-    
-@app.route("/create_attendence_form")
-def create_attendence_form():
-    return render_template('create_attendance.html')
+    return render_template("admin/add_stud.html",data=data)
 
-
-@app.route("/attendance_module")
-def attendance_module():
-    return render_template('attendance_module.html')
-
-@app.route("/create_attendence",methods=["POST","GET"])
-def create_attendence():
-    in_name = request.form['name']
-    in_section = request.form['section']
-    in_id = request.form['id']
+@app.route('/del_attendance/<string:classname>', methods=["POST", "GET"])
+def del_attendance(classname):
     con = sqlite3.connect("data_base.db")
     cur = con.cursor()
-    query = "CREATE TABLE IF NOT EXISTS {}(rollno integer primary key,name varchar(50),all_periods integer,p1 integer,p2 integer,p3 integer,p4 integer,p5 integer,p6 integer,p7 integer,p8 integer,how_present int,how_absent int)".format(in_name)
+    query = f"DROP TABLE {classname}"
+    cur.execute("DELETE FROM all_classes WHERE classname = ?", (classname,))
     cur.execute(query)
     con.commit()
-    cur.execute("insert into all_attendance(name,section,id)values(?,?,?)",(in_name,in_section,in_id))
-    con.commit()
     con.close()
+    return "Deleted successfully"
+
+
+
+ 
+@app.route('/staff_view_attendance/<string:classname>', methods=["POST", "GET"])
+def staff_view_attendance(classname):
     con = sqlite3.connect("data_base.db")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("Select * from all_attendance")
+    query = f"SELECT * FROM {classname}"
+    cur.execute(query)
     data = cur.fetchall()
     con.close()
-    return render_template("all_attendance.html", data=data)
-
-
-
-@app.route("/all_attendance")
-def all_attendance():
-    con = sqlite3.connect("data_base.db")
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("Select * from all_attendance")
-    data = cur.fetchall()
-    con.close()
-    return render_template("all_attendance.html", data=data)
-
-@app.route("/view_attendance/<string:name>")
-def view_attendance(name):
-    return render_template('attendance_sheet.html',name=name)
-
-@app.route("/add_student/<string:name>")
-def add_student(name):
-    return render_template('add_student_form.html',name=name)
-
-@app.route("/insert_student",methods=["POST","GET"])
-def insert_student():
-    in_roll = request.form['roll_no']
-    in_name = request.form['stud_name']
-    in_att_name = request.form['attendance_name']
-    con = sqlite3.connect("data_base.db")
-    cur = con.cursor()
-    cur.execute( "INSERT INTO nantha (rollno,name) VALUES(?,?)",(in_roll,in_name))
-    con.commit()
-    con.close()
-    return "inserted"
-
-
-
+    return render_template("staff/add_stud.html",data=data)
 
 
 if(__name__ == '__main__'):
     app.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''@app.route("/add_contact")
-def add_contact():
-    return render_template("add_contact.html")
-
-@app.route("/insert_contact",methods=["POST","GET"])
-def insert_contact():
-    try:
-        in_name = request.form['name']
-        in_pno = request.form['pno']
-        in_email = request.form['email']
-        in_address = request.form['add']
-        con = sqlite3.connect("db_contact_details.db")
-        cur = con.cursor()
-        cur.execute("INSERT INTO tb_contact_detail(name,pno,email,address) VALUES(?,?,?,?)", (in_name,in_pno,in_email,in_address))
-        con.commit()
-    except:
-        return "<h1>The Contact Already Exist </h1>"
-    finally:
-        return render_template('added_success.html')
-        con.close()
-
-@app.route('/view_contacts')
-def view_contacts():
-    con = sqlite3.connect("db_contact_details.db")
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-  cur.execute("Select * from tb_contact_detail")
-    data = cur.fetchall()
-    con.close()  
-    return render_template("contacts.html",data=data)
-
-
-@app.route('/update_contact/<string:id>',methods=["POST","GET"])
-def update_contact(id):
-    con = sqlite3.connect("db_contact_details.db")
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("SELECT * FROM tb_contact_detail where pk=?",(id))
-    data = cur.fetchone()
-    con.commit()
-    return render_template("update_contact.html",data=data)
-    con.close()
-
-@app.route('/update_contact_db',methods=["POST","GET"])
-def update_contact_db():
-    try:
-        in_name = request.form['name']
-        in_pno = request.form['pno']
-        in_email = request.form['email']
-        in_address = request.form['add']
-        con = sqlite3.connect("db_contact_details.db")
-        cur = con.cursor()
-        cur.execute("UPDATE tb_contact_detail SET name=?,pno=?,email=?,address=? WHERE pno= ?" ,(in_name,in_pno,in_email,in_address,in_pno))
-        con.commit()
-    except:
-        return "<h1> Updation Error</h1>"
-    finally:
-        return render_template('update_success.html')
-
-@app.route('/delete_contact/<string:id>')
-def delete_contact(id):
-    con = sqlite3.connect("db_contact_details.db")
-    cur = con.cursor()
-    cur.execute("delete from tb_contact_detail where pk=?",(id))
-    con.commit()
-    return render_template('delete_success.html')
-    con.close()
-'''
