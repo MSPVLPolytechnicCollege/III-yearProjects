1e726b7e0db617c894da978ed0dc4d214248e968
PooranaSelvan <pooranaselvan67@gmail.com>
2025-02-22 22:52:16 +0530

    axios instance

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "http://localhost:5000/api",
   baseURL: "https://e-com-backend-vev8.onrender.com/api",
   withCredentials: true,
 });
