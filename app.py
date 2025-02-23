ac10bbfeddecdbc78225618f435ac498eac44cca
ShriHarini09 <ce23503858@gmail.com>
2025-02-23 13:10:23 +0530

    CODE WAS ADDED

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash, session,jsonify
 import sqlite3
+import base64 #Base64 use pannurathu browser-la image store & display panna best option.
+
 
 app = Flask(__name__)
 
 app.secret_key="123"
 
+UPLOAD_FOLDER='static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 @app.route('/login')
@@ -27,6 +31,7 @@ def stuhome():
 
 @app.route('/success')
 def success():
+    message = request.args.get('message', 'Operation Successful!')  # Default msg if not provided
     return render_template('success.html')
 
 con = sqlite3.connect('alumini_db.db')
@@ -47,9 +52,10 @@ con.execute("""
 #contact table
 con = sqlite3.connect('alumini_db.db')
 con.execute("""
-    CREATE TABLE IF NOT EXISTS contact (
+    CREATE TABLE IF NOT EXISTS contact1 (
+        sno INTEGER PRIMARY KEY AUTOINCREMENT,
         stu_name  text,
-        stu_email TEXT PRIMARY KEY,
+        stu_email TEXT,
         stu_contact integer,
         subject TEXT,
         message TEXT
@@ -59,12 +65,13 @@ con.execute("""
 #course table
 con = sqlite3.connect('alumini_db.db')
 con.execute("""
-    CREATE TABLE IF NOT EXISTS course_college (
+    CREATE TABLE IF NOT EXISTS course (
         sno INTEGER PRIMARY KEY AUTOINCREMENT,
         course TEXT ,
         active text
     );
 """)
+
 
 #alumni register table
 con = sqlite3.connect('alumini_db.db')
@@ -80,8 +87,19 @@ con.execute("""
         address text
     );
 """)
+con.close()
 
 
+#gallery
+con=sqlite3.connect('alumini_db.db')
+con.execute("""
+    CREATE TABLE if not exists image1 (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    image BLOG,
+    description text
+    );
+""")
+con.close()
 
 con=sqlite3.connect('alumini_db.db')
 con.execute("create table if not exists admin1(username text primary key,password text);")
@@ -182,7 +200,13 @@ def alumni_login():
 
 @app.route('/admin')
 def admin_dashboard():
-    return render_template('admin.html')
+    total_events=get_total_events() #get total events count edukka
+    total_contact=get_total_contact() #get total contact count 
+
+    print("Total Events:", total_events)  # Debugging
+    print("Total Contact:", total_contact)  # Debugging
+
+    return render_template('admin.html',total_events=total_events,total_contact=total_contact)
 
 
 #admin login page to verify
@@ -380,7 +404,7 @@ def stucontact():
         message = request.form['contact-message']
         con = sqlite3.connect('alumini_db.db')
         cursor = con.cursor()
-        cursor.execute('insert into contact(stu_name,stu_email,stu_contact,subject,message) values(?,?,?,?,?)',(name,email,contact,subject,message))
+        cursor.execute('insert into contact1(stu_name,stu_email,stu_contact,subject,message) values(?,?,?,?,?)',(name,email,contact,subject,message))
         con.commit()
         con.close()
         return redirect(url_for('stuhome'))
@@ -393,18 +417,151 @@ def view_contact():
     con = sqlite3.connect('alumini_db.db')
     con.row_factory = sqlite3.Row  # Enables dictionary-style access
     cursor = con.cursor()
-    cursor.execute('select * from contact')
+    cursor.execute('select * from contact1')
     res = cursor.fetchall()
     return render_template('contact-view.html',datas=res)
 
-@app.route('/course')
+#delete the contact by the admin
+@app.route('/deleteevent/<string:sno>',methods=['GET','POST'])
+def deletecontact(sno):
+    con=sqlite3.connect('alumini_db.db')
+    cursor=con.cursor()
+    cursor.execute('delete from contact1 where sno=?',(sno,))
+    con.commit()
+    con.close()
+    return redirect(url_for('contact-view.html'))
+
+#to view the courses
+@app.route('/course', methods=['GET', 'POST'])
 def course():
     con = sqlite3.connect('alumini_db.db')
     con.row_factory = sqlite3.Row  # Enables dictionary-style access
     cursor = con.cursor()
-    cursor.execute('select * from course_college')
+    cursor.execute('select * from course')
     res = cursor.fetchall()
-    return render_template("manage-course.html", datas=res)
+    con.close()
+    return render_template('manage_course.html',datas=res)
+
+
+#Delete the course by admin
+@app.route('/deletecourse/<string:sno>',methods=['GET','POST'])
+def deletecourse(sno):
+    con=sqlite3.connect('alumini_db.db')
+    cursor=con.cursor()
+    cursor.execute('delete from course where sno=?',(sno,))
+    con.commit()
+    con.close()
+    return redirect(url_for('course'))
+
+#gallery
+@app.route('/gallery')
+def gallery():
+    con = sqlite3.connect('alumini_db.db')
+    cursor = con.cursor()
+    cursor.execute('SELECT image,description FROM image1')  # Fetching image data
+    images = cursor.fetchall()
+    con.close()
+
+    # Convert images to Base64 format for display
+    image_list = [
+        {
+            'data': f"data:image/jpeg;base64,{base64.b64encode(img[0]).decode('utf-8')}",
+            'description':img[1]
+        }
+        for img in images
+    ]
+
+    return render_template('add-gallery.html', images=image_list)
+
+
+#student and alumni gallery
+@app.route('/stu_gallery')
+def stu_gallery():
+    con = sqlite3.connect('alumini_db.db')
+    cursor = con.cursor()
+    cursor.execute('SELECT image,description FROM image1')  # Fetching image data
+    images = cursor.fetchall()
+    con.close()
+
+     # Convert images to Base64 format for display
+    image_list = [
+        {
+            'data': f"data:image/jpeg;base64,{base64.b64encode(img[0]).decode('utf-8')}",
+            'description':img[1]
+        }
+        for img in images
+    ]
+    return render_template('stu-alumni-gallery.html', images=image_list)
+
+
+#gallery - data to be add
+@app.route('/insert_image',methods=['GET','POST'])
+def insert_image():
+    if request.method=='POST':
+        if 'image' not in request.files: #Request-la 'image' field irukka-nu check panrom.
+            return "No file part" #If image file upload aagala-na, "No file part" nu solli return pannidum.
+        
+        image=request.files['image'] #User upload panna image-a eduthuttu image variable-la store panrom.
+        description=request.form['description']
+
+
+        if image.filename == '':
+            return "No selected file"
+        
+        # Convert image to binary
+        image_data = image.read()
+
+        con=sqlite3.connect('alumini_db.db')
+        cursor=con.cursor()
+        cursor.execute('insert into image1(image,description) values(?,?)',(image_data,description)) #E.g., static/uploads/example.jpg nu path DB-la store aagum.
+        con.commit()
+        con.close()
+        print("Redirecting to /gallery")  # Debugging log
+        return redirect(url_for('gallery'))
+    return render_template('/add-gallery.html')
+
+#event count dynamic ka update in the admin page
+@app.route('/get_total_events')
+def get_total_events():
+    conn = sqlite3.connect('alumini_db.db')
+    cursor = conn.cursor()
+
+    #COUNT(*) → Table la ethana row iruku nu count pannum.
+    cursor.execute("SELECT COUNT(*) FROM event")
+
+    #cursor.fetchone() → One row fetch pannum (Database la result ah eduthukum).
+    #[0] → First column value ah eduthukum (COUNT result ah eduthukum).
+    total_events = cursor.fetchone()[0]  # Event count return pannum
+
+    conn.close()
+    return total_events
+
+#javascript or ajax request pana json format the data va return panro
+@app.route('/get_total_events_json')
+def get_total_events_json():
+    return jsonify({'total': get_total_events()})  # JSON format la return pannum
+
+#contact  count dynamic ka update in the admin page
+@app.route('/get_total_contact')
+def get_total_contact():
+    conn = sqlite3.connect('alumini_db.db')
+    cursor = conn.cursor()
+
+    #COUNT(*) → Table la ethana row iruku nu count pannum.
+    cursor.execute("SELECT COUNT(*) FROM contact1")
+
+    #cursor.fetchone() → One row fetch pannum (Database la result ah eduthukum).
+    #[0] → First column value ah eduthukum (COUNT result ah eduthukum).
+    total_contact = cursor.fetchone()[0]  # Event count return pannum
+
+    conn.close()
+    return total_contact
+
+#javascript or ajax request pana json format the data va return panro
+@app.route('/get_total_contact_json')
+def get_total_contact_json():
+    return jsonify({'total': get_total_contact()})  # JSON format la return pannum
+
 
 
 if __name__=='__main__':
